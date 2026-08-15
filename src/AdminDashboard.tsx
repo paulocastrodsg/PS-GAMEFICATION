ad83f17ec8bee59243ec379fb2ce046f1fa8d356
paulocastrodsg <paulocastrodsg@gmail.com>
2026-08-14 22:58:42 -0300

    Criado dashboard Admin

---
+++ src/AdminDashboard.tsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard() {
+    return (
+      <div>
+        <h1>Dashboard do administrador</h1>
+        <p>Aqui vai aparecer o resultado de todos os usuários e as configurações.</p>
+      </div>
+    );
+  }
